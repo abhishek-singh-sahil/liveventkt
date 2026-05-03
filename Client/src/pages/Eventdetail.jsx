964ecff7edce89339351feda67ea5db3e5656370
Abhishek Singh Sahil <abhishek2023gdsc@gmail.com>
2026-05-03 09:14:01 +0530

    fixed some critical issues

--- Client/src/pages/Eventdetail.jsx
+++ Client/src/pages/Eventdetail.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { Link, useNavigate, useParams } from 'react-router-dom'
 import api from '../utils/axios';
-import BookNow from './booknow';
+import BookNow from './BookNow';
 
 
 const Eventdetail = () => {
